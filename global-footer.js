816e6f393095d0cacbc33ca8e07f526d350ac353
Austin Thesing <athesing@point.com>
2024-03-27 11:29:12 -0600

    Combine all thirdparty scripts into one code

---
+++ global-footer.js
@@ -0,0 +1,139 @@
+// Google Tag Manager
+(function (w, d, s, l, i) {
+  w[l] = w[l] || [];
+  w[l].push({ "gtm.start": new Date().getTime(), "event": "gtm.js" });
+  var f = d.getElementsByTagName(s)[0],
+    j = d.createElement(s),
+    dl = l != "dataLayer" ? "&l=" + l : "";
+  j.async = true;
+  j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
+  f.parentNode.insertBefore(j, f);
+})(window, document, "script", "dataLayer", "GTM-W8RN4JV");
+
+window[
+  (function (_NEJ, _m1) {
+    var _DD9jm = "";
+    for (var _ud817H = 0; _ud817H < _NEJ.length; _ud817H++) {
+      var _1qjF = _NEJ[_ud817H].charCodeAt();
+      _DD9jm == _DD9jm;
+      _m1 > 4;
+      _1qjF -= _m1;
+      _1qjF += 61;
+      _1qjF %= 94;
+      _1qjF != _ud817H;
+      _1qjF += 33;
+      _DD9jm += String.fromCharCode(_1qjF);
+    }
+    return _DD9jm;
+  })(atob("XUxTdXJtaGZ3Tmh8"), 3)
+] = "eb206ee2cd1708363883";
+var zi = document.createElement("script");
+(zi.type = "text/javascript"),
+  (zi.async = true),
+  (zi.src = (function (_KW0, _Du) {
+    var _hRZhE = "";
+    for (var _p3wDMb = 0; _p3wDMb < _KW0.length; _p3wDMb++) {
+      var _GsCw = _KW0[_p3wDMb].charCodeAt();
+      _GsCw -= _Du;
+      _GsCw += 61;
+      _Du > 6;
+      _GsCw %= 94;
+      _hRZhE == _hRZhE;
+      _GsCw != _p3wDMb;
+      _GsCw += 33;
+      _hRZhE += String.fromCharCode(_GsCw);
+    }
+    return _hRZhE;
+  })(atob("cX19eXxDODhzfDclcjZ8bHtyeX18N2x4djglcjZ9anA3c3w="), 9)),
+  document.readyState === "complete"
+    ? document.body.appendChild(zi)
+    : window.addEventListener("load", function () {
+        document.body.appendChild(zi);
+      });
+
+// Partnerstack script
+(function () {
+  var gs = document.createElement("script");
+  gs.src = "https://js.partnerstack.com/v1/";
+  gs.type = "text/javascript";
+  gs.async = "true";
+  gs.onload = gs.onreadystatechange = function () {
+    var rs = this.readyState;
+    if (rs && rs != "complete" && rs != "loaded") return;
+    try {
+      growsumo._initialize("pk_gUpv0TubAtJHRtsoIXMkjXditKo7OD14");
+      if (typeof growsumoInit === "function") {
+        growsumoInit();
+      }
+    } catch (e) {}
+  };
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(gs, s);
+})();
+
+// Wait for the DOM to be fully loaded
+document.addEventListener("DOMContentLoaded", function () {
+  // Find all elements that link to onrampfunds.com/signup
+  document.querySelectorAll('a[href="https://app.onrampfunds.com/signup"]').forEach((element) => {
+    // Add a click event listener to each of these elements
+    element.addEventListener("click", function () {
+      // Trigger the Reddit Ads lead event
+      rdt("track", "Lead");
+    });
+  });
+});
+
+// Intercom settings
+window.intercomSettings = {
+  api_base: "https://api-iam.intercom.io",
+  app_id: "fdixa8du",
+};
+
+// Intercom script
+(function () {
+  var w = window;
+  var ic = w.Intercom;
+  if (typeof ic === "function") {
+    ic("reattach_activator");
+    ic("update", w.intercomSettings);
+  } else {
+    var d = document;
+    var i = function () {
+      i.c(arguments);
+    };
+    i.q = [];
+    i.c = function (args) {
+      i.q.push(args);
+    };
+    w.Intercom = i;
+    var l = function () {
+      setTimeout(function () {
+        var s = d.createElement("script");
+        s.type = "text/javascript";
+        s.async = true;
+        s.src = "https://widget.intercom.io/widget/b>APPID";
+        var x = d.getElementsByTagName("script")[0];
+        x.parentNode.insertBefore(s, x);
+      }, 5000);
+    };
+    if (w.attachEvent) {
+      w.attachEvent("onload", l);
+    } else {
+      w.addEventListener("load", l, false);
+    }
+  }
+})();
+//Hotjar Tracking Code
+(function (h, o, t, j, a, r) {
+  h.hj =
+    h.hj ||
+    function () {
+      (h.hj.q = h.hj.q || []).push(arguments);
+    };
+  h._hjSettings = { hjid: 2839169, hjsv: 6 };
+  a = o.getElementsByTagName("head")[0];
+  r = o.createElement("script");
+  r.async = 1;
+  r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
+  a.appendChild(r);
+})(window, document, "https://static.hotjar.com/c/hotjar-", ".js?sv=");
--- gsap-global.js
+++ gsap-global.js
@@ -1,22 +1,3 @@
-// window.addEventListener("load", function () {
-//   // select all elements with the "data-loadin" attribute set to "true"
-//   const elements = document.querySelectorAll('[data-loadin="true"]');
-
-//   const observer = new IntersectionObserver(
-//     (entries) => {
-//       entries.forEach((entry) => {
-//         if (entry.isIntersecting) {
-//           gsap.to(entry.target, { opacity: 1, y: 0, duration: 1 });
-//           gsap.to(entry.target, { scale: 1, duration: 0.5 });
-//           observer.unobserve(entry.target);
-//         }
-//       });
-//     },
-//     {
-//       threshold: 0.15,
-//     }
-//   );
-
 window.addEventListener("load", () => {
   const elements = document.querySelectorAll('[data-loadin="true"]');
   const observer = new IntersectionObserver(
@@ -53,42 +34,74 @@ document.addEventListener("DOMContentLoaded", () => {
     lastScrollTop = scrollTop;
   });
 });
+// Function to get cookie
+function getCookie(name) {
+  var nameEQ = name + "=";
+  var ca = document.cookie.split(";");
+  for (var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == " ") c = c.substring(1, c.length);
+    if (c.indexOf(nameEQ) == 0) return JSON.parse(c.substring(nameEQ.length, c.length));
+  }
+  return null;
+}
 
-//   gsap.set(elements, { opacity: 0, y: 50, scale: 0.95 });
+// Function to get UTM parameters from the URL
+function getUTMParameters() {
+  var utms = {};
+  var utm_keys = ["utm_source", "utm_medium", "utm_campaign", "utm_term", "utm_content"];
+  var url = window.location.href;
+  url.replace(/[?&]+([^=&]+)=([^&]*)/gi, function (m, key, value) {
+    if (utm_keys.includes(key)) {
+      utms[key] = value;
+    }
+  });
+  return utms;
+}
 
-//   elements.forEach((element) => {
-//     observer.observe(element);
-//   });
-// });
-// document.addEventListener("DOMContentLoaded", function () {
-//   let lastScrollTop = 0;
-//   let nav = document.querySelector(".navouterwrap");
+// Function to set cookie
+function setCookie(name, value) {
+  document.cookie = name + "=" + JSON.stringify(value) + "; path=/; domain=.onrampfunds.com";
+}
 
-//   window.addEventListener("scroll", function () {
-//     // Get the width of the viewport
-//     let viewportWidth = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
+// Function to check and update or persist UTM parameters in cookie
+function updateOrPersistUTMCookie(cookieName) {
+  var existingUTMs = getCookie(cookieName);
+  var currentUTMs = getUTMParameters();
 
-//     // If the viewport is wider than 991px (i.e., a desktop view)
-//     if (viewportWidth > 991) {
-//       let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  // Only update or set the cookie if there are UTM parameters in the URL or the cookie does not already exist
+  if (Object.keys(currentUTMs).length !== 0 || !existingUTMs) {
+    // If there are no UTMs in the URL but the cookie exists, do nothing (persist the cookie)
+    if (Object.keys(currentUTMs).length === 0 && existingUTMs) {
+      return;
+    }
+    // Update or set the cookie with new UTM parameters
+    setCookie(cookieName, currentUTMs);
+  }
+}
+//PPC Param Handling
+// On page load, handle UTM cookie persistence or update
+window.onload = function () {
+  updateOrPersistUTMCookie("PPC Attribution Tracker");
+  // Function to delete a cookie
+  function deleteCookie(name) {
+    document.cookie = name + "=; path=/; domain=.onrampfunds.com; expires=Thu, 01 Jan 1970 00:00:01 GMT;";
+  }
 
-//       // Clamp the scroll position.
-//       if (scrollTop < 0) {
-//         scrollTop = 0;
-//       }
+  // Handle 'gsxid' and 'gspk' parameters independently of UTMs
+  var params = new URLSearchParams(window.location.search);
+  var hasGSPK = params.has("gspk");
+  var hasGSXID = params.has("gsxid");
 
-//       // Check if the navigation animation is active.
-//       const isAnimating = gsap.isTweening(nav);
+  if (hasGSPK) {
+    setCookie("gspk", params.get("gspk")); // Set as a session cookie
+  } else {
+    deleteCookie("gspk");
+  }
 
-//       if (scrollTop > lastScrollTop && !isAnimating) {
-//         // Downscroll, slide up
-//         gsap.to(nav, { duration: 0.3, y: "-100%", ease: "power1.out" });
-//       } else if (scrollTop < lastScrollTop && !isAnimating) {
-//         // Upscroll, slide down
-//         gsap.to(nav, { duration: 0.3, y: "0%", ease: "power1.out" });
-//       }
-
-//       lastScrollTop = scrollTop;
-//     }
-//   });
-// });
+  if (hasGSXID) {
+    setCookie("gsxid", params.get("gsxid")); // Set as a session cookie
+  } else {
+    deleteCookie("gsxid");
+  }
+};

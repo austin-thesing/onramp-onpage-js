d2db63bfe2d2be9e20bcd6d3d1135d64f3876a30
Austin Thesing <austin@designxdevelop.com>
2025-06-27 15:41:21 -0600

    Change file permissions to executable for footer and backup files; comment out Intercom settings in global-footer.js

--- global-footer.js
+++ global-footer.js
@@ -83,46 +83,46 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
-// Intercom settings
-window.intercomSettings = {
-  api_base: "https://api-iam.intercom.io",
-  app_id: "fdixa8du",
-};
+// // Intercom settings
+// window.intercomSettings = {
+//   api_base: "https://api-iam.intercom.io",
+//   app_id: "fdixa8du",
+// };
 
-// Intercom script
-(function () {
-  var w = window;
-  var ic = w.Intercom;
-  if (typeof ic === "function") {
-    ic("reattach_activator");
-    ic("update", w.intercomSettings);
-  } else {
-    var d = document;
-    var i = function () {
-      i.c(arguments);
-    };
-    i.q = [];
-    i.c = function (args) {
-      i.q.push(args);
-    };
-    w.Intercom = i;
-    var l = function () {
-      setTimeout(function () {
-        var s = d.createElement("script");
-        s.type = "text/javascript";
-        s.async = true;
-        s.src = "https://widget.intercom.io/widget/b>APPID";
-        var x = d.getElementsByTagName("script")[0];
-        x.parentNode.insertBefore(s, x);
-      }, 5000);
-    };
-    if (w.attachEvent) {
-      w.attachEvent("onload", l);
-    } else {
-      w.addEventListener("load", l, false);
-    }
-  }
-})();
+// // Intercom script
+// (function () {
+//   var w = window;
+//   var ic = w.Intercom;
+//   if (typeof ic === "function") {
+//     ic("reattach_activator");
+//     ic("update", w.intercomSettings);
+//   } else {
+//     var d = document;
+//     var i = function () {
+//       i.c(arguments);
+//     };
+//     i.q = [];
+//     i.c = function (args) {
+//       i.q.push(args);
+//     };
+//     w.Intercom = i;
+//     var l = function () {
+//       setTimeout(function () {
+//         var s = d.createElement("script");
+//         s.type = "text/javascript";
+//         s.async = true;
+//         s.src = "https://widget.intercom.io/widget/b>APPID";
+//         var x = d.getElementsByTagName("script")[0];
+//         x.parentNode.insertBefore(s, x);
+//       }, 5000);
+//     };
+//     if (w.attachEvent) {
+//       w.attachEvent("onload", l);
+//     } else {
+//       w.addEventListener("load", l, false);
+//     }
+//   }
+// })();
 //Hotjar Tracking Code
 (function (h, o, t, j, a, r) {
   h.hj =
